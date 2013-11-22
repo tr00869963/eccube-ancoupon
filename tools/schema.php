41e15f8d1d5169a83c15a4f11d45a43a1e7a4f1b
Seiji Nitta <zenith6@gmail.com>
2013-11-22 23:38:52 +0900

    ビルドツールを修正

--- tools/schema.php
+++ tools/schema.php
@@ -16,19 +16,19 @@ require_once "{$eccube_html_dir}/require.php";
 $query = SC_Query_Ex::getSingletonInstance();
 
 $tables = array(
-    'plg_AnCoupon_coupon' => array(),
-    'plg_AnCoupon_coupon_discount_rule' => array(),
-    'plg_AnCoupon_discount_rule' => array(),
-    'plg_AnCoupon_discount_rule_category' => array(),
-    'plg_AnCoupon_discount_rule_product' => array(),
-    'plg_AnCoupon_discount_rule_product_class' => array(),
-    'plg_AnCoupon_order_coupon' => array(),
+    'plg_ancoupon_coupon' => array(),
+    'plg_ancoupon_coupon_discount_rule' => array(),
+    'plg_ancoupon_discount_rule' => array(),
+    'plg_ancoupon_discount_rule_category' => array(),
+    'plg_ancoupon_discount_rule_product' => array(),
+    'plg_ancoupon_discount_rule_product_class' => array(),
+    'plg_ancoupon_order_coupon' => array(),
 );
 
 $sequences = array(
-    'plg_AnCoupon_coupon_coupon_id' => 1,
-    'plg_AnCoupon_discount_code' => 1,
-    'plg_AnCoupon_discount_rule_id' => 1,
+    'plg_ancoupon_coupon_coupon_id' => 1,
+    'plg_ancoupon_discount_code' => 1,
+    'plg_ancoupon_discount_rule_id' => 1,
 );
 
 $schema = AN_Eccube_DbUtils::buildDatabaseSchema($query, $tables, $sequences);
